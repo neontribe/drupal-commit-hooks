08dfc2767c485be0b4cc8ee6e7972ff8a6014bf5
Toby Batch <tobias@neontribe.co.uk>
2016-02-25 10:41:58 +0000

    Initial import

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+composer global require squizlabs/php_codesniffer
+composer global require drupal/coder
+phpcs --config-set installed_paths $HOME/.composer/vendor/drupal/coder/coder_sniffer
+
+path=`php -r "echo realpath(getcwd());"`
+ln -sf $path/contrib/pre-commit $path/.git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+
+which doxygen > /dev/null
+
+if [ "$?" != 0 ]; then
+    echo
+    echo "Skipping document generation, doxygen not installed"
+    echo "  sudo apt-get install doxygen"
+else
+    doxygen contrib/doxygen.conf
+fi
+
+
