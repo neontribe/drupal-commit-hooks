6dd4422efcba11cb3d77a64e6f56ea321e8beb39
Toby Batch <tobias@neontribe.co.uk>
2016-08-09 10:00:44 +0100

    Remove doxygen from setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 composer global require squizlabs/php_codesniffer
 composer global require drupal/coder
+composer global require graphp/graphviz
+
+export PATH=$PATH:$HOME/.composer/vendor/bin/
 phpcs --config-set installed_paths $HOME/.composer/vendor/drupal/coder/coder_sniffer
 
 path=`php -r "echo realpath(getcwd());"`
@@ -15,7 +18,7 @@ if [ "$?" != 0 ]; then
     echo "Skipping document generation, doxygen not installed"
     echo "  sudo apt-get install doxygen"
 else
-    doxygen contrib/doxygen.conf
+    echo doxygen contrib/doxygen.conf
 fi
 
 
